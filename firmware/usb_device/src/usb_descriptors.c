2a3335f96550ffb872969ba743359d6da1634fe6
Nico Bollen <nico.bollen@gmail.com>
2026-01-07 14:15:40 +0100

    (temp) add cdc to device descriptor

--- firmware/usb_device/src/usb_descriptors.c
+++ firmware/usb_device/src/usb_descriptors.c
@@ -59,10 +59,38 @@ tusb_desc_device_t const device_descriptor =
  * Configuration descriptor
  * -------------------------------------------------------------------- */
 
+#define EPNUM_CDC_NOTIF_OUT 0x01
+#define EPNUM_CDC_IN 0x02
+#define EPNUM_CDC_OUT 0x03
 #define EPNUM_VENDOR_IN 0x04
 #define EPNUM_VENDOR_OUT 0x05
 
-#define TUSB_DESC_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_VENDOR_DESC_LEN)
+#define TUSB_DESC_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN + TUD_VENDOR_DESC_LEN)
+
+/* CDC Descriptor Template copied from usbd.h to be able to update CDC ACM bmCapabilities to 6 */
+#define TUD_CDC_DESCRIPT_2(_itfnum, _stridx, _ep_notif, _ep_notif_size, _epout, _epin, _epsize) \
+        /* Interface Associate */ \
+        8, TUSB_DESC_INTERFACE_ASSOCIATION, _itfnum, 2, TUSB_CLASS_CDC, CDC_COMM_SUBCLASS_ABSTRACT_CONTROL_MODEL, \
+        CDC_COMM_PROTOCOL_NONE, 0, \
+        /* CDC Control Interface */ \
+        9, TUSB_DESC_INTERFACE, _itfnum, 0, 1, TUSB_CLASS_CDC, CDC_COMM_SUBCLASS_ABSTRACT_CONTROL_MODEL, \
+        CDC_COMM_PROTOCOL_NONE, _stridx, \
+        /* CDC Header */ \
+        5, TUSB_DESC_CS_INTERFACE, CDC_FUNC_DESC_HEADER, U16_TO_U8S_LE(0x0120), \
+        /* CDC Call */ \
+        5, TUSB_DESC_CS_INTERFACE, CDC_FUNC_DESC_CALL_MANAGEMENT, 0, (uint8_t)((_itfnum) + 1), \
+        /* CDC ACM: support line request */ \
+        4, TUSB_DESC_CS_INTERFACE, CDC_FUNC_DESC_ABSTRACT_CONTROL_MANAGEMENT, 6, \
+        /* CDC Union */ \
+        5, TUSB_DESC_CS_INTERFACE, CDC_FUNC_DESC_UNION, _itfnum, (uint8_t)((_itfnum) + 1), \
+        /* Endpoint Notification */ \
+        7, TUSB_DESC_ENDPOINT, _ep_notif, TUSB_XFER_INTERRUPT, U16_TO_U8S_LE(_ep_notif_size), 16, \
+        /* CDC Data Interface */ \
+        9, TUSB_DESC_INTERFACE, (uint8_t)((_itfnum) + 1), 0, 2, TUSB_CLASS_CDC_DATA, 0, 0, 0, \
+        /* Endpoint Out */ \
+        7, TUSB_DESC_ENDPOINT, _epout, TUSB_XFER_BULK, U16_TO_U8S_LE(_epsize), 0, \
+        /* Endpoint In */ \
+        7, TUSB_DESC_ENDPOINT, _epin, TUSB_XFER_BULK, U16_TO_U8S_LE(_epsize), 0
 
 /** HID configuration descriptor */
 uint8_t const configuration_descriptor[] = {
@@ -72,8 +100,15 @@ uint8_t const configuration_descriptor[] = {
                           TUSB_DESC_TOTAL_LEN,  /* total length */
                           TUSB_DESC_CONFIG_ATT_SELF_POWERED,  /* attribute */
                           100),                 /* power in mA */
+    TUD_CDC_DESCRIPT_2(ITF_NUM_CDC,                         /* interface number */
+                       4,                                   /* string index */
+                       0x80 | EPNUM_CDC_NOTIF_OUT,          /* EP notification address */
+                       8,                                   /* EP notification size */
+                       EPNUM_CDC_OUT,                       /* EP Out address */
+                       0x80 | EPNUM_CDC_IN,                 /* EP In address */
+                       64),                                 /* size */
     TUD_VENDOR_DESCRIPTOR(ITF_NUM_VENDOR,                   /* interface number */
-                          4,                                /* string index */
+                          5,                                /* string index */
                           EPNUM_VENDOR_OUT,                 /* EP Out address */
                           0x80 | EPNUM_VENDOR_IN,           /* EP In address */
                           64)                               /* size */
@@ -154,12 +189,12 @@ uint8_t const ms_os_20_descriptor[] = {
     /* PropertyName ("DeviceInterfaceGUIDs\0" in UTF-16) */
     'D', 0x00, 'e', 0x00, 'v', 0x00, 'i', 0x00, 'c', 0x00, 'e', 0x00, 'I', 0x00, 'n', 0x00, 't', 0x00, 'e', 0x00, 'r',
     0x00, 'f', 0x00, 'a', 0x00, 'c', 0x00, 'e', 0x00, 'G', 0x00, 'U', 0x00, 'I', 0x00, 'D', 0x00, 's', 0x00, 0x00, 0x00,
-    U16_TO_U8S_LE(0x0050),                          /* wPropertyDataLength */
-    /* PropertyData ("{3A1C31C1-32C8-49D8-8DDD-FB6C1819D30A}\0\0") */
-    '{', 0x00, '3', 0x00, 'A', 0x00, '1', 0x00, 'C', 0x00, '3', 0x00, '1', 0x00, 'C', 0x00, '1', 0x00, '-', 0x00, '3',
-    0x00, '2', 0x00, 'C', 0x00, '8', 0x00, '-', 0x00, '4', 0x00, '9', 0x00, 'D', 0x00, '8', 0x00, '-', 0x00, '8', 0x00,
-    'D', 0x00, 'D', 0x00, 'D', 0x00, '-', 0x00, 'F', 0x00, 'B', 0x00, '6', 0x00, 'C', 0x00, '1', 0x00, '8', 0x00, '1',
-    0x00, '9', 0x00, 'D', 0x00, '3', 0x00, '0', 0x00, 'A', 0x00, '}', 0x00,
+    U16_TO_U8S_LE(0x0050),                                  /* wPropertyDataLength */
+    /* PropertyData ("{3223A062-CAA8-436E-B92B-820140357447}\0\0") */
+    '{', 0x00, '3', 0x00, '2', 0x00, '2', 0x00, '3', 0x00, 'A', 0x00, '0', 0x00, '6', 0x00, '2', 0x00, '-', 0x00, 'C',
+    0x00, 'A', 0x00, 'A', 0x00, '8', 0x00, '-', 0x00, '4', 0x00, '3', 0x00, '6', 0x00, 'E', 0x00, '-', 0x00, 'B', 0x00,
+    '9', 0x00, '2', 0x00, 'B', 0x00, '-', 0x00, '8', 0x00, '2', 0x00, '0', 0x00, '1', 0x00, '4', 0x00, '0', 0x00, '3',
+    0x00, '5', 0x00, '7', 0x00, '4', 0x00, '4', 0x00, '7', 0x00, '}', 0x00,
     0x00, 0x00, 0x00, 0x00
 };
 
@@ -176,6 +211,7 @@ enum {
     STRID_MANUFACTURER,
     STRID_PRODUCT,
     STRID_SERIAL,
+    STRID_CDC_INT,
     STRID_WEBUSB_INT
 };
 
@@ -190,7 +226,8 @@ char const * string_descriptor[] = {
     manufacturerName,       /* 1: Manufacturer */
     deviceDescription,      /* 2: Product */
     device_serial_nr,       /* 3: serial number */
-    "MCM WebUSB"            /* 4: Vendor */
+    "MCM CDC",              /* 4: CDC */
+    "MCM WebUSB"            /* 5: Vendor */
 };
 
 
